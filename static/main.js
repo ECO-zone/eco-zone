4335c0218afec227ebf9894c9ae8b87d7f38b216
Kevin Brochet-Nguyen <k.brochet-nguyen@qantic.com>
2024-06-06 11:28:52 +0200

    Add cron

--- static/main.js
+++ static/main.js
@@ -51,80 +51,6 @@ Highcharts.setOptions({
 });
 
 Highcharts.chart('chart-timeseries-redispatch', {
-    title: {
-        text: 'Redispatch-Leistung als Timeseries',
-        align: 'left'
-    },
-
-    subtitle: {
-        text: 'Mittlere Leistung in MW pro Richtung. Hochrechnung durch ECO zone. Datenquelle: <a href="https://www.netztransparenz.de/de-de/Systemdienstleistungen/Betriebsf%C3%BChrung/Redispatch" target="_blank">Netztransparenz.de</a>.',
-        align: 'left'
-    },
-
-    yAxis: {
-        title: {
-            text: 'Mittlere Leistung MW'
-        }
-    },
-
-    xAxis: {
-        title: {
-            text: 'Auflösung: 15-Minuten'
-        },
-        accessibility: {
-            rangeDescription: 'Auflösung: 15-Minuten'
-        }
-    },
-
-    legend: {
-        layout: 'horizontal',
-        align: 'left',
-        verticalAlign: 'bottom'
-    },
-    tooltip: {
-        shared: true,
-    },
-
-    // plotOptions: {
-    //     series: {
-    //         label: {
-    //             connectorAllowed: false
-    //         },
-    //         pointStart: 2010
-    //     }
-    // },
-
-    // series: [{
-    //     name: 'Installation & Developers',
-    //     data: [
-    //         43934, 48656, 65165, 81827, 112143, 142383,
-    //         171533, 165174, 155157, 161454, 154610
-    //     ]
-    // }, {
-    //     name: 'Manufacturing',
-    //     data: [
-    //         24916, 37941, 29742, 29851, 32490, 30282,
-    //         38121, 36885, 33726, 34243, 31050
-    //     ]
-    // }, {
-    //     name: 'Sales & Distribution',
-    //     data: [
-    //         11744, 30000, 16005, 19771, 20185, 24377,
-    //         32147, 30912, 29243, 29213, 25663
-    //     ]
-    // }, {
-    //     name: 'Operations & Maintenance',
-    //     data: [
-    //         null, null, null, null, null, null, null,
-    //         null, 11164, 11218, 10077
-    //     ]
-    // }, {
-    //     name: 'Other',
-    //     data: [
-    //         21908, 5548, 8105, 11248, 8989, 11816, 18274,
-    //         17300, 13053, 11906, 10073
-    //     ]
-    // }],
     credits: {
         enabled: false,
     },
@@ -139,19 +65,33 @@ Highcharts.chart('chart-timeseries-redispatch', {
             }
         },
     },
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                }
-            }
-        }]
-    }
-
+    legend: {
+        layout: 'horizontal',
+        align: 'left',
+        verticalAlign: 'bottom'
+    },
+    subtitle: {
+        text: 'Mittlere Leistung in MW pro Richtung. Hochrechnung durch ECO zone. Datenquelle: <a href="https://www.netztransparenz.de/de-de/Systemdienstleistungen/Betriebsf%C3%BChrung/Redispatch" target="_blank">Netztransparenz.de</a>.',
+        align: 'left'
+    },
+    title: {
+        text: 'Redispatch-Leistung als Timeseries',
+        align: 'left'
+    },
+    tooltip: {
+        shared: true,
+    },
+    xAxis: {
+        title: {
+            text: 'Auflösung: 15-Minuten'
+        },
+        accessibility: {
+            rangeDescription: 'Auflösung: 15-Minuten'
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Mittlere Leistung MW'
+        }
+    },
 });
