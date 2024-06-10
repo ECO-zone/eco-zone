d1fd03491f8b5da1d55cbfcbda7982f3e9de98d4
Kevin Brochet-Nguyen <k.brochet-nguyen@qantic.com>
2024-06-10 15:59:11 +0200

    Use wget instead of curl

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -6,7 +6,7 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # Notify Sentry of the new release
-curl https://us.sentry.io/api/hooks/release/builtin/4507407118893056/88770a9376df40d110be8845180139f9f28b707df7672136841e7406f26b9090/ \
-  -X POST \
-  -H 'Content-Type: application/json' \
-  -d "{\"version\": \"${GIT_REV}\"}"
+wget --header="Content-Type: application/json" \
+  --post-data="{\"version\": \"${GIT_REV}\"}" \
+  --output-document - \
+  $SENTRY_RELEASE_URL
